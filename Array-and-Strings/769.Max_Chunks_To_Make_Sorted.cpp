898e3f9d64ee78674fb2322a4524f39da39608d1
NH709 <harshkedia600@gmail.com>
2022-01-02 11:01:36 +0530

    added max chunks part2

--- Array-and-Strings/769.Max_Chunks_To_Make_Sorted.cpp
+++ Array-and-Strings/769.Max_Chunks_To_Make_Sorted.cpp
@@ -21,3 +21,29 @@ public:
 };
 
 // another solution is to keep sum and indexsum variable and check when they become equal then increase the chunks
+
+// best approach is using leftmax and rightmin
+class Solution
+{
+public:
+    int maxChunksToSorted(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> rightMin(n + 1, INT_MAX);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            rightMin[i] = min(arr[i], rightMin[i + 1]);
+        }
+        int leftMax = INT_MIN;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            leftMax = max(leftMax, arr[i]);
+            if (leftMax <= rightMin[i + 1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
