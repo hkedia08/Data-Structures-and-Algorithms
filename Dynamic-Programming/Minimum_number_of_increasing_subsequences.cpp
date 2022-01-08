d37be299781e34104db277184ff7997eb149cc8b
NH709 <harshkedia600@gmail.com>
2022-01-08 19:12:34 +0530

    added Minimum_number_of_increasing_subsequences

---
+++ Dynamic-Programming/Minimum_number_of_increasing_subsequences.cpp
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/minimum-number-of-increasing-subsequences/
+
+// Possible solutions of this problem
+
+// Method 1
+// reverse the vector and find the Length of LIS
+
+// Method 2
+// multiply each element by -1 and find the LIS
+
+// Method 3
+// find the longest decreasing subsequence
+
+// Implementation is same as LIS
