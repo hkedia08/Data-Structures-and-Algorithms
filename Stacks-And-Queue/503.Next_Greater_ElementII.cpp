9f46f1a512e6ef1e68bf67c864288a5d5a2036e0
NH709 <harshkedia600@gmail.com>
2022-01-11 09:36:11 +0530

    added new approach Next_Greater_ElementII

--- Stacks-And-Queue/503.Next_Greater_ElementII.cpp
+++ Stacks-And-Queue/503.Next_Greater_ElementII.cpp
@@ -32,3 +32,39 @@ public:
         return result;
     }
 };
+
+// another approach
+class Solution
+{
+public:
+    vector<int> nextGreaterElements(vector<int> &nums)
+    {
+        stack<int> st;
+        vector<int> result;
+        int n = nums.size();
+        // first push all the elements in to the stack so it now easy to find ngt for last element
+        // no need to use concept of circular arrary
+        for (int i = n - 1; i >= 0; i--)
+        {
+            st.push(nums[i]);
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (st.size() > 0 && st.top() <= nums[i])
+            {
+                st.pop();
+            }
+            if (st.size() == 0)
+            {
+                result.push_back(-1);
+            }
+            else
+            {
+                result.push_back(st.top());
+            }
+            st.push(nums[i]);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
